f941b3a8e1334d11a3f37319ed8a35f72d8fab00
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2022-06-07 13:52:19 -0300

    Create VisualMatrix.cs

---
+++ Elements/VisualMatrix.cs
@@ -0,0 +1,11 @@
+using System.Drawing;
+
+namespace VisualLogic.Elements;
+
+public class VisualMatrix : VisualElement
+{
+    protected override void Draw(Bitmap bmp, Graphics g)
+    {
+        throw new System.NotImplementedException();
+    }
+}
